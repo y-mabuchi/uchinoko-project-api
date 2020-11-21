1f7df5c51003c174075b2cd650bb31ea2bc6b7b3
gonta1026 <katano.original@gmail.com>
2020-11-21 13:51:16 +0900

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
+import express, { Request, Response, NextFunction } from "express";
+interface MesageRequest extends Request {
+  body: {
+    message: number
+  }
 }
+const app = express();
+app.use(express.json());
 
-console.log(hello("TypeScript"));
+app.get("/", (req: MesageRequest, res: Response, next: NextFunction): void => {
+  console.log(req.body.message)
+  res.send(`<h1>Hello World</h1>`);
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+});
+app.listen(3000);
